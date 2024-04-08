832fe9e77f1fb3720101af3efb03fdeee6742c1d
Guilherme Rodniski Correia <guilherme.correia@rodoparana.com.br>
2024-04-08 14:36:04 -0300

    COMPANY

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Notification from "./Notification";
-import CompanyLogos from "./CompanyLogos";
+import { companyLogos } from "../constants";
 
-const Hero = () => {
+const Hero = (className) => {
   const parallaxRef = useRef(null);
 
   return (
@@ -81,9 +81,22 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
-
+      <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Ajudando pessoas a encontrarem soluções agrícolas em
+      </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+            key={index}
+          >
+            <img src={logo} width={134} height={28} alt={logo} />
+          </li>
+        ))}
+      </ul>
+    </div>
       <BottomLine />
     </Section>
   );
